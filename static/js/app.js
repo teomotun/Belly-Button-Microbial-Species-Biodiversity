f94f50d2c4f6e90ba63ef758350f7074ac465fba
tolu <teomotun@ncsu.edu>
2020-03-13 11:03:23 -0400

    Updated app.js

--- static/js/app.js
+++ static/js/app.js
@@ -41,7 +41,7 @@ function init() {
 };
 
 
-// On change to the DOM, call getData()
+// On change to the DOM, call optionChanged()
 d3.selectAll("#selDataset").on("change", optionChanged);
 
 // Function called by DOM changes
@@ -49,26 +49,59 @@ function optionChanged() {
   var dropdownMenu = d3.select("#selDataset");
   // Assign the value of the dropdown menu option to a variable
   var dataset = dropdownMenu.property("value");
-  // Initialize an empty array for the country's data
+  // Initialize an empty array for the query data
   var data = [];
 
-  if (dataset == 'dataset1') {
-    data = us;
-  }
-  else if (dataset == 'dataset2') {
-    data = uk;
-  }
-  else if (dataset == 'dataset3') {
-    data = canada;
-  }
-  // Call function to update the chart
-  updatePlotly(data);
-}
+  // if (dataset == 'dataset1') {
+  //   data = us;
+  // }
+  // else if (dataset == 'dataset2') {
+  //   data = uk;
+  // }
+  // else if (dataset == 'dataset3') {
+  //   data = canada;
+  // }
 
-// Update the restyled plot's values
-function updatePlotly(newdata) {
-  Plotly.restyle("pie", "values", [newdata]);
-}
+
+  var name = Data["names"];
+  // for (var i = 0; i < name.length; i++) {
+  //   if (name[i] == dataset) {
+  //     console.log(name[i]);
+  //     var n = i;
+  //   };
+  // };
+  console.log(name)
+};
+  // switch (dataset) {
+  //   case "dataset1":
+  //     x = [1, 2, 3, 4, 5];
+  //     y = [1, 2, 4, 8, 16];
+  //     break;
+
+  //   case "dataset2":
+  //     x = [10, 20, 30, 40, 50];
+  //     y = [1, 10, 100, 1000, 10000];
+  //     break;
+
+  //   case "dataset3":
+  //     x = [100, 200, 300, 400, 500];
+  //     y = [10, 100, 50, 10, 0];
+  //     break;
+
+  //   default:
+  //     x = [1, 2, 3, 4, 5];
+  //     y = [1, 2, 3, 4, 5];
+  //     break;
+
+
+//   // Call function to update the chart
+//   updatePlotly(data);
+// }
+
+// // Update the restyled plot's values
+// function updatePlotly(newdata) {
+//   Plotly.restyle("pie", "values", [newdata]);
+// }
 
 
 
@@ -178,4 +211,4 @@ function showmetadata(n) {
 //       x = [1, 2, 3, 4, 5];
 //       y = [1, 2, 3, 4, 5];
 //       break;
-//   }
+// //   }
